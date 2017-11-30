463217e1cddcabe9ab07891e94000494f039cef4
Robin Meis <robin@meis.space>
2017-11-30 14:13:36 +0100

    Test outpu

--- probes2mqtt/probes2mqtt.py
+++ probes2mqtt/probes2mqtt.py
@@ -4,6 +4,6 @@ clientprobes = set()
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11ProbeReq):
         if len(pkt.info) > 0:
-            print(pkt.addr2)
+            print(pkt.addr2 + " -> " + pkt.addr1 + " : " + pkt.info.decode("UTF-8"))
 
 sniff (iface="alfamon", prn=PacketHandler)
